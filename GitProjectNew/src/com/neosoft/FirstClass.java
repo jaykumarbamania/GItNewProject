406b1a3f123dabf96a0ce0f99547ea7998faebb4
jaykumarbamania <90679000+jaykumarbamania@users.noreply.github.com>
2021-09-16 12:44:00 +0530

    Second Commit

--- GitProjectNew/src/com/neosoft/FirstClass.java
+++ GitProjectNew/src/com/neosoft/FirstClass.java
@@ -4,7 +4,12 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		
+String branch = "CE";
 		
+		if(branch =="CE")
+			System.out.println("Computer Engineer");
+		else
+			System.out.println("Not a Computer Engineer");
 	}
 
 }
