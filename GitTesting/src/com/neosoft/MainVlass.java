03127cd3b8978729dde69c408519de373f37096a
jaykumarbamania <90679000+jaykumarbamania@users.noreply.github.com>
2021-09-16 11:56:01 +0530

    Second Commit

--- GitTesting/src/com/neosoft/MainVlass.java
+++ GitTesting/src/com/neosoft/MainVlass.java
@@ -4,7 +4,8 @@ public class MainVlass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Welcome");
+		System.out.print("Git is nice");
+		System.out.print("Hello World");
 	}
 
 }
